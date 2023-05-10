ed37cf3ebdde762537b2b438231cb9c3335c1623
Ryan Sawadichai <ryan.sawadichai@outlook.com>
2023-05-09 20:57:22 -0600

    2.9 phonebook filtering exercise

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,65 +1,101 @@
-import { useState } from 'react'
-import "./App.css"
+import { useState } from "react";
+import "./App.css";
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas',
-      num: '040-1234567'}
-  ]) 
-  const [newName, setNewName] = useState('')
-  const [newNum, setNewNum] = useState('')
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
+  ]);
 
-  const addPerson = (event) => {
+  const [newName, setNewName] = useState("");
+  const [newNum, setNewNum] = useState("");
+  const [filterText, setFilter] = useState("");
+
+  function addPerson(event) {
     event.preventDefault();
 
-    if (!newName || !newNum){
+    if (!newName || !newNum) {
       console.log("Nothing entered");
-      return ;
+      return;
     }
     // check if name is in array already
-    const isInArray = persons.some((person) => person.name === newName)
-    if (isInArray){
-      alert(`${newName} is already added to phonebook.`)
-      return ;
+    const isInArray = persons.some((person) => person.name === newName);
+    if (isInArray) {
+      alert(`${newName} is already added to phonebook.`);
+      return;
     }
 
     setPersons(() => {
-      let newPersons = persons.concat({name: newName, num: newNum});
+      let newPersons = persons.concat({
+        name: newName,
+        number: newNum,
+        id: persons.length,
+      });
       setNewName("");
       setNewNum("");
       return newPersons;
-    })
+    });
   }
 
-  const handleNameChange = (event) => {
+  function handleNameChange(event) {
     setNewName(event.target.value);
   }
-  const handleNumChange = (event) => {
+  function handleNumChange(event) {
     setNewNum(event.target.value);
   }
+  function handleFilterChange(event) {
+    const newFilterText = event.target.value.toLowerCase();
+    setFilter(newFilterText);
+  }
+
+  const filteredEntries = filterText
+    ? // if filterText is not empty
+      // return person that contains filterText
+      persons.filter((person) => {
+        return (
+          person.name.toLowerCase().includes(filterText) ||
+          person.number.includes(filterText)
+        );
+      })
+    : // else return the whole obj
+      persons;
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form >
+      <form action="">
         <div>
-          name: <input type="text" value={newName} onChange={handleNameChange} />
-        </div>
-        <div>
-          num: <input type="tel" value={newNum} onChange={handleNumChange} />
-        </div>
-        <div>
-          <button type="submit" onClick={addPerson}>add</button>
+          filter shown with{" "}
+          <input type="text" value={filterText} onChange={handleFilterChange} />
         </div>
       </form>
-      <h2>Numbers</h2>
+      <h2>Add New Phonebook Entry</h2>
+      <form>
+        <div>
+          name:{" "}
+          <input type="text" value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          number: <input type="tel" value={newNum} onChange={handleNumChange} />
+        </div>
+        <div>
+          <button type="submit" onClick={addPerson}>
+            add
+          </button>
+        </div>
+      </form>
+      <h2>Phonebook Entries</h2>
       <ul>
-        {persons.map((person) => (
-          <li key={person.name} className="phonebook-numbers">{person.name} {person.num}</li>
+        {filteredEntries.map((person) => (
+          <li key={person.name} className="phonebook-numbers">
+            {person.name} {person.number}
+          </li>
         ))}
       </ul>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
