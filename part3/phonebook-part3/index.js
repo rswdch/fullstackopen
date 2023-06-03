0dcf523be3540cbc59daff5e3d9eb0c4acf6af2c
Ryan Sawadichai <ryan.sawadichai@outlook.com>
2023-06-02 19:35:54 -0600

    3a 3.4-3.6 phonebook delete post

--- part3/phonebook-part3/index.js
+++ part3/phonebook-part3/index.js
@@ -50,13 +50,56 @@ app.get("/api/persons/:id", (request, response) => {
   console.log("Getting Person");
   const id = Number(request.params.id);
   const person = persons.find((entry) => entry.id === id);
-  console.log(person);
   if (!person) {
     return response.status(400).json({
       error: "person not found",
     });
   }
   response.json(person);
+});
+
+// DELETE Request
+app.delete("/api/persons/:id", (request, response) =>{
+  const id = Number(request.params.id);
+  // Check if person exists. Not necessary but will return error if person does not exist.
+  // const person = persons.find((entry) => entry.id === id);
+  // if (!person) {
+  //   return response.status(400).json({
+  //     error: "person not found",
+  //   });
+  // }
+  persons = persons.filter(entry => {
+    return entry.id !== id;
+  })
+  response.json(persons);
+})
+
+// POST Request
+const generateId = () => {
+  return Math.round(Math.random() * 10000000);
+}
+
+app.post('/api/persons', (request, response) => {
+  // Check that all fields are filled
+  if (!request.body.name || !request.body.number) {
+    return response.status(400).json({
+      error: "content missing",
+    })
+  }
+  // check if person already exists
+  if (persons.find((entry) => entry.name === request.body.name)){
+    return response.status(400).json({
+      error: "name must be unique",
+    })
+  }
+
+  const newPerson = {
+    id: generateId(),
+    name: request.body.name,
+    number: request.body.number,
+  }
+  persons = persons.concat(newPerson);
+  response.json(newPerson);
 });
 
 // Start server
